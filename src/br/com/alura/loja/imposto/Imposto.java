1a2ba15905df2a6998455bea18805af13b6f9a0d
diogo <diogocorrea@tecnomotor.com.br>
2022-06-16 18:11:42 -0300

    2. Mais de um imposto com Decorator
    O padrão Decorator permite adicionar novos comportamentos a um objeto, tornando o código bastante flexível e dinâmico, pois esses comportamentos podem ser adicionados em tempo de execuçaõ

--- src/br/com/alura/loja/imposto/Imposto.java
+++ src/br/com/alura/loja/imposto/Imposto.java
@@ -4,8 +4,27 @@ import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.Orcamento;
 
-public interface Imposto {
+// transformada em classe para ter outros métodos com implementação
+public abstract class Imposto {
 
-	BigDecimal calcular(Orcamento orcamento);
+	// classe que representa outro imposto - autorelacionamento
+	// uma classe está decorando outra classe com ela mesma
+	private Imposto outro;
+	
+	public Imposto(Imposto outro) {
+		this.outro = outro;
+	}
+
+	// abstrato, pois não sabe quem vai calcular
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImposto = realizarCalculo(orcamento);
+		BigDecimal valorDoOutroImposto = BigDecimal.ZERO;
+		// se não tiver outro imposto então o valor é zero
+		if (outro != null)
+			valorDoOutroImposto = outro.realizarCalculo(orcamento);
+		return valorImposto.add(valorDoOutroImposto);
+	}
 
 }
